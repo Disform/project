a04c7b9d8245f5714d89f107c0fe827b8235c380
Disform <disform22@gmail.com>
2022-04-30 12:20:11 +0300

    iteration

--- JS1/script.js
+++ JS1/script.js
@@ -12,3 +12,18 @@ const personalMovieDB = {
 const answerFilmsName = prompt('Какой фильм вы смотрели последним?', '');
 const answerFilmsGrade = promt('Сколько балов из 10 вы ему поставили бы?', '');
 personalMovieDB.movies[answerFilmsName] = answerFilmsGrade;
+
+console.log(!1 && 2 || !3 );
+
+let result = '';
+
+for (let i = 0; i < 4; i++) {
+    result += '*';
+    i++;
+    console.log(result);
+    for (let j = 0; j < 3; j++){
+        result += '*';
+        j++;
+        console.log(result);
+    }
+}
