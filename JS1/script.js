328c35976d159f2cf1ea9393cc1cce4d36f8674e
Disform <disform22@gmail.com>
2022-04-30 08:29:03 +0300

    Первый урок

--- JS1/script.js
+++ JS1/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-let number = 5;
-const number2 = 10;
-number = 6;
-console.log (number);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const answerFilmsName = prompt('Какой фильм вы смотрели последним?', '');
+const answerFilmsGrade = promt('Сколько балов из 10 вы ему поставили бы?', '');
+personalMovieDB.movies[answerFilmsName] = answerFilmsGrade;
