9434ac37c401f178f44bfeaccad618d75dc59cf2
Disform <disform22@gmail.com>
2022-05-02 17:49:01 +0300

    урок 29 практика с функциями

--- JS1/script.js
+++ JS1/script.js
@@ -9,21 +9,55 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const answerFilmsName = prompt('Какой фильм вы смотрели последним?', '');
-const answerFilmsGrade = promt('Сколько балов из 10 вы ему поставили бы?', '');
-personalMovieDB.movies[answerFilmsName] = answerFilmsGrade;
 
-console.log(!1 && 2 || !3 );
-
-let result = '';
-
-for (let i = 0; i < 4; i++) {
-    result += '*';
-    i++;
-    console.log(result);
-    for (let j = 0; j < 3; j++){
-        result += '*';
-        j++;
-        console.log(result);
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Какой фильм вы посмотрели последним?', ''),
+              b = prompt('Какую оценку вы ему поставите?', '');
+        if (a.length < 50 && a != '' && a != null && b != null && b != '') {
+            personalMovieDB[a] = b;
+            } else {
+                i--;
+            }
     }
 }
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('мало посмотрел');
+    } else if (personalMovieDB.count > 30) {
+        console.log('вы киноман');
+    } else if (personalMovieDB >= 10 && personalMovieDB <= 30) {
+        console.log('обычный пользователь');
+    } else {
+        console.log('ошибка')
+    }
+}
+detectPersonalLevel();
+
+
+// здесь идёт проверка нашего privat на то true ли он. 
+function showMyDB(hidden) {
+    if (!hidden) {
+    console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++){
+        const a = prompt(`Ваш любимый жанр под номером ${i}`);
+        if (a !== null && a !== undefined){
+            personalMovieDB.genres[i - 1] = a;
+        } else {
+            i--;
+        }
+    }
+}
+// можно было сделать через 
+// personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);  минуя переменную а, но я я изначально подумал что нужна будет проверка
+// а как сделать её без доп переменной я не в курсе.
+
+
